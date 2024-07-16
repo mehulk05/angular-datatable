0f634869baf6b7daa605e8eb051f784f5369b67a
mehul kothari <mehulkothari05@gmail.com>
2024-07-16 20:05:18 +0530

    changes ffor datatable  with angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { DataTableDirective } from 'angular-datatables';
+import { Api, Config } from 'datatables.net';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'angular-18-v1';
+export class AppComponent implements OnInit, OnDestroy {
+  @ViewChild(DataTableDirective, { static: false }) dtElement!: DataTableDirective;
+  dtOptions: Config = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+  posts: any = [];
+  searchTerm: string = '';
+
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      destroy: true
+    };
+
+    // Fetch initial data
+    this.fetchData();
+  }
+
+  fetchData(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe((posts: any) => {
+        this.posts = posts;
+        this.dtTrigger.next(null);
+      });
+  }
+
+  searchPosts(): void {
+    if (this.searchTerm.trim() === '') {
+      this.fetchData(); // If search term is empty, fetch all data
+    } else {
+      this.http.get(`https://jsonplaceholder.typicode.com/posts?title_like=${this.searchTerm}`)
+        .subscribe((posts: any) => {
+          this.posts = posts;
+          this.rerender();
+        });
+    }
+  }
+
+  rerender(): void {
+    this.dtElement.dtInstance.then((dtInstance: Api) => {
+      dtInstance.destroy();
+      this.dtTrigger.next(null);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    DataTablesModule,
     AppRoutingModule
   ],
   providers: [],
